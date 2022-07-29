14e8a89b289bde3bd12d3504b37372930eebf65c
jeffersonfarias <jeffarbar@yahoo.com.br>
2022-07-29 11:38:07 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,20 @@
 import io
 
 from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
 from response import Response
 
 import comparacao
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post('/compara')
 async def compara(foto_1: bytes = File(...),
