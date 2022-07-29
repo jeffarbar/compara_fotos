7fe5e3a4f48710c509745ef62463c25aeec3e902
jeffersonfarias <jeffarbar@yahoo.com.br>
2022-07-28 23:20:58 -0300

    Add files via upload

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import io
 
 from fastapi import FastAPI, File
-from starlette.responses import StreamingResponse
+from response import Response
 
 import comparacao
 
@@ -12,15 +12,22 @@ app = FastAPI()
 async def compara(foto_1: bytes = File(...),
                   foto_2: bytes = File(...)) -> str:
 
-    img_1 = io.BytesIO(foto_1)
-    img_1.seek(0)
+    try:
+        img_1 = io.BytesIO(foto_1)
+        img_1.seek(0)
 
-    img_2 = io.BytesIO(foto_2)
-    img_2.seek(0)
+        img_2 = io.BytesIO(foto_2)
+        img_2.seek(0)
 
-    result = comparacao.compare(img_1, img_2)
+        result = comparacao.compare(img_1, img_2)
 
-    if result:
-        return {"result": "Deu match"}
-    else:
-        return {"result": "Não deu match"}
+        if result:
+            res = Response(0, "Deu Match")
+        else:
+            res = Response(0, "Não Deu Match")
+
+        return res
+
+    except Exception as ex:
+        return Response(1, ex)
+
